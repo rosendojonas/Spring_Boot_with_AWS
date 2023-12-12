508baf634053eaf37ef60e5411b0ca4dcc855a1f
Jonas Rosendo <jonasrosendo1@gmail.com>
2023-12-12 00:36:18 -0300

    First commit

---
+++ src/main/kotlin/com/jonasrosendo/aws_api/AwsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.jonasrosendo.aws_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwsApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/jonasrosendo/aws_api/AwsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jonasrosendo.aws_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
