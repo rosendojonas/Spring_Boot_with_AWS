d840ef18cac3b56529e6f7514a858581751a457b
Jonas Rosendo <jonasrosendo1@gmail.com>
2023-12-12 02:01:25 -0300

    adding role to user

--- src/main/kotlin/com/jonasrosendo/aws_api/domain/enums/models/User.kt
+++ src/main/kotlin/com/jonasrosendo/aws_api/domain/enums/models/User.kt
@@ -1,10 +1,13 @@
 package com.jonasrosendo.aws_api.domain.enums.models
 
+import com.jonasrosendo.aws_api.domain.enums.Role
+
 data class User(
     val id: Long,
     val name: String,
     val email: String,
     val password: String,
+    val role: Role,
     val requests: List<Request> = arrayListOf(),
     val stages: List<RequestStage> = arrayListOf()
 )
